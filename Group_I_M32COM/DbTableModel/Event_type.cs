00a6d8cfb2fd60b470f5358faf8bcdc311a48694
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-03-15 12:54:59 +0000

    Setting validations for event_type model;

--- Group_I_M32COM/DbTableModel/Event_type.cs
+++ Group_I_M32COM/DbTableModel/Event_type.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace Group_I_M32COM.DbTableModel
     public class Event_type
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter type of event")]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "The minimum {2} and Maximum {1} characters are allowed", MinimumLength = 3)]
+        [Display(Name = "Event Type Name")]
         public string Event_type_name { get; set; }
         public DateTime? Created_At { get; set; }
         public DateTime? Updated_At { get; set; }
