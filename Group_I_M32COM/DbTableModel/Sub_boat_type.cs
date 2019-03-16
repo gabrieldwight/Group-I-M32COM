245dc428ffad41c56e82cc2c18abc7343eec8454
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-03-16 21:00:27 +0300

    Added display name to the sub type class

--- Group_I_M32COM/DbTableModel/Sub_boat_type.cs
+++ Group_I_M32COM/DbTableModel/Sub_boat_type.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,11 @@ namespace Group_I_M32COM.DbTableModel
     public class Sub_boat_type
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter sub boat category")]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "The minimum {2} and Maximum {1} characters are allowed", MinimumLength = 3)]
+        [Display(Name = "Sub Boat Class Category")]
         public string Sub_boat_class_type { get; set; }
         public DateTime? Created_At { get; set; }
         public DateTime? Updated_At { get; set; }
