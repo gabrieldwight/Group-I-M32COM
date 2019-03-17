cb7467cf5872d37f92a42063a72a4026f6c29874
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-03-17 22:53:48 +0300

    Added event participation model and database table

---
+++ Group_I_M32COM/DbTableModel/Event_participation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_I_M32COM.DbTableModel
+{
+    // This is a table model to register the boat team participating in the event.
+    public class Event_participation
+    {
+        public int Id { get; set; }
+
+        public string position { get; set; }
+
+        public int points_awarded { get; set; }
+
+        [Display(Name = "Date Added")]
+        public DateTime? Created_At { get; set; }
+
+        [Display(Name = "Date Updated")]
+        public DateTime? Updated_At { get; set; }
+
+        // Foreign key for Event table
+        [Display(Name = "Type of event")]
+        public Event Event { get; set; }
+
+        // Foreign key for Boat Crew table
+        [Display(Name = "Boat Team")]
+        public Boat_crew boat_Crew { get; set; }
+    }
+}
