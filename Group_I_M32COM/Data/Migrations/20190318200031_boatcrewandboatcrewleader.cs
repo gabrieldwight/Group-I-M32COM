ffe5e0f519fa5183f1eaf47905c3492db695285d
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-03-19 17:15:12 +0300

    Added foreign key for the boat crew leader table

---
+++ Group_I_M32COM/Data/Migrations/20190318200031_boatcrewandboatcrewleader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group_I_M32COM.Data.Migrations
+{
+    public partial class boatcrewandboatcrewleader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boat_crew_leader",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    User_Id = table.Column<string>(nullable: true),
+                    Created_At = table.Column<DateTime>(nullable: true),
+                    Updated_At = table.Column<DateTime>(nullable: true),
+                    boat_CrewId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boat_crew_leader", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Boat_crew_leader_Boat_Crews_boat_CrewId",
+                        column: x => x.boat_CrewId,
+                        principalTable: "Boat_Crews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boat_crew_leader_boat_CrewId",
+                table: "Boat_crew_leader",
+                column: "boat_CrewId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boat_crew_leader");
+        }
+    }
+}
