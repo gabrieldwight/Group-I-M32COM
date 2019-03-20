01e8942816d3ae4c58e1371d51607a04ce97807b
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-03-20 03:24:14 +0300

    Created event API controller by Umair Contribution

---
+++ Group_I_M32COM/Controllers/EventsAPIController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Group_I_M32COM.Data;
+using Group_I_M32COM.DbTableModel;
+
+namespace Group_I_M32COM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EventsAPI
+        [HttpGet]
+        public IEnumerable<Event> GetEvents()
+        {
+            // To return the linked table objects to the API as a json string for the calendar 
+            return _context.Events.Include(et => et.Event_Types).Include(bt => bt.Boat_Types).Include(ep => ep.Event_Participations);
+        }
+
+        // GET: api/EventsAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEvent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        // PUT: api/EventsAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvent([FromRoute] int id, [FromBody] Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != @event.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@event).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EventsAPI
+        [HttpPost]
+        public async Task<IActionResult> PostEvent([FromBody] Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Events.Add(@event);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEvent", new { id = @event.Id }, @event);
+        }
+
+        // DELETE: api/EventsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            _context.Events.Remove(@event);
+            await _context.SaveChangesAsync();
+
+            return Ok(@event);
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.Id == id);
+        }
+    }
+}
